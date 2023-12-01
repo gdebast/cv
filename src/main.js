b6c6722f9d587ba7636818c4042aebe04867e946
gdebast <gatien.debast@gmail.com>
2023-12-01 19:29:11 +0100

    clean

--- src/main.js
+++ src/main.js
@@ -22,19 +22,3 @@ dplMovieRuntimePool.addRuntime(
 
 // canvas test
 createBouncingCircles(15, 1, 5);
-
-const g5 = new DirectedGraph();
-const g5n1 = g5.createNode("n1");
-const g5n2 = g5.createNode("n2");
-const g5n3 = g5.createNode("n3");
-const g5n4 = g5.createNode("n4");
-g5.createArc(g5n1, g5n2, "a1");
-g5.createArc(g5n2, g5n1, "a2");
-g5.createArc(g5n2, g5n3, "a3");
-g5.createArc(g5n2, g5n4, "a4");
-g5.createArc(g5n3, g5n4, "a5");
-g5.createArc(g5n4, g5n3, "a6");
-
-g5.computeLevelsClustersAndCycles(true);
-
-console.log(g5);
